a58459955e5d9e646af0097e2b90aa432e55d47a
blopax <pablo42clement@gmail.com>
2019-01-09 02:10:01 +0100

    ex04 finished

--- d02/ex04/elem.py
+++ d02/ex04/elem.py
@@ -1,15 +1,16 @@
 class Text(str):
-    def __init__(self, input=''):
-        if input is None:
+    def __init__(self, text_input=''):
+        super().__init__()
+        if text_input is None:
             self.text = ''
         else:
-            self.text = str(input)
+            self.text = str(text_input)
 
     def text_replace(self):
         clean_text = self.text
-        #clean_text = clean_text.replace("<", "&lt;")
-        #clean_text = clean_text.replace(">", "&gt;")
-        #clean_text = clean_text.replace('"', "&quot;")
+        clean_text = clean_text.replace("<", "&lt;")
+        clean_text = clean_text.replace(">", "&gt;")
+        clean_text = clean_text.replace('"', "&quot;")
         clean_text = clean_text.replace("\n", "\n<br />\n")
         return clean_text
 
@@ -18,138 +19,62 @@ class Text(str):
 
 
 class Elem:
-    def __init__(self, tag='div', attr={}, content='', tag_type='double'):
-        # name = str, attribute = dict, content = string, type =string)
+    class ValidationError(Exception):
+        def __str__(self):
+            return "ValidationError"
+
+    def __init__(self, tag='div', attr=None, content=None, tag_type='double'):
         self.tag = tag
+        if attr is None:
+            attr = {}
         self.attribute = attr
-        self.content = str(Text(content)) #=self.parse_content(content) ou ger si nul ou pas et si liste ou pas on peut faire un liste append pour ajouter a liste vide
-        if self.content != '':
-            self.content = "\n  " + self.content + "\n"
+        self.content_list = self._content_to_list(content)
         self.tag_type = tag_type
+        self.string_content = self._parse_content()
+
+    @staticmethod
+    def _content_to_list(content):
+        content_list = []
+        if content is None:
+            content_list = [Text("")]
+        elif type(content) != list:
+            content_list.append(content)
+        else:
+            content_list = content
+        return content_list
+
+    def _parse_content(self):
+        content_string = ""
+        for content_item in self.content_list:
+            if type(content_item) not in [Elem, Text]:
+                raise self.ValidationError()
+            if str(content_item) != '':
+                content_string += str("\n" + str(content_item))
+        if content_string != "":
+            content_string = content_string.replace("\n", "\n  ")
+            content_string += "\n"
+        return content_string
+
+    def add_content(self, new_content):
+        new_content_list = self._content_to_list(new_content)
+        self.content_list += new_content_list
+        self.string_content = self._parse_content()
 
     def __str__(self):
         attribute_str = ''
         for k, v in self.attribute.items():
             attribute_str += " {}={}".format(k, v)
         if self.tag_type == 'double':
-            html = "<{}{}>{}</{}>".format(self.tag, attribute_str, self.content, self.tag)
+            html = "<{}{}>{}</{}>".format(self.tag, attribute_str, self.string_content, self.tag)
         else:
             html = "<{}{}/>".format(self.tag, attribute_str)
-        print(html)
         return html
 
 
-
-def test_text():
-    # What is Text?
-    assert isinstance(Text(), str)
-    # Default behaviour :
-    assert str(Text()) == ''
-    # With an argument :
-    assert str(Text('')) == ''
-    assert str(Text('foo')) == 'foo'
-    # Pattern replacing :
-    assert str(Text('\n')) == '\n<br />\n'
-    assert str(Text('foo\nbar')) == 'foo\n<br />\nbar'
-    # Escaping <, >, "...
-    assert str(Text('<')) == '&lt;'
-    assert str(Text('>')) == '&gt;'
-    assert str(Text('"')) == '&quot;'
-    print('Text behaviour : OK.')
-
-def test_elem_basics():
-    # Default behaviour :
-    assert str(Elem()) == '<div></div>'
-    # Arguments order :
-    assert str(Elem('div', {}, None, 'double')) == '<div></div>'
-    # Argument names :
-    assert str(Elem(tag='body', attr={}, content=Elem(),
-                    tag_type='double')) == '<body>\n  <div></div>\n</body>'
-    # With elem as content :
-    assert str(Elem(content=Elem())) == '<div>\n  <div></div>\n</div>'
-    # With list as content :
-    assert str(Elem(content=[Text('foo'), Text('bar'), Elem()])) == '<div>\n  foo\n  bar\n \
- <div></div>\n</div>'
-    print('Basic Elem behaviour : OK.')
-
-
-def test_empty_texts():
-    assert str(Elem(content=Text(''))) == '<div></div>'
-    assert str(Elem(content=[Text(''), Text('')])) == '<div></div>'
-    assert str(Elem(content=[Text('foo'), Text(''), Elem()])) == '<div>\n  foo\
-\n  <div></div>\n</div>'
-    print('Elem with empty texts : OK.')
-
-
-def test_errors():
-    # Type error if the content isn't made of Text or Elem.
-    try:
-        Elem(content=1)
-    except Exception as e:
-        assert type(e) == Elem.ValidationError
-    # The right way :
-    assert str(Elem(content=Text(1))) == '<div>\n  1\n</div>'
-
-    # Type error if the elements of the list aren't Text or Elem instances.
-    try:
-        Elem(content=['foo', Elem(), 1])
-    except Exception as e:
-        assert type(e) == Elem.ValidationError
-    # The right way :
-    assert (str(Elem(content=[Text('foo'), Elem(), Text(1)]))
-            == '<div>\n  foo\n  <div></div>\n  1\n</div>')
-
-    # Same with add_method()
-    try:
-        elem = Elem()
-        elem.add_content(1)
-        raise (Exception("incorrect behaviour."))
-    except Exception as e:
-        assert isinstance(e, Elem.ValidationError)
-
-    # Or with lists :
-    try:
-        elem = Elem()
-        elem.add_content([1, ])
-        raise (Exception('incorrect behaviour'))
-    except Exception as e:
-        assert isinstance(e, Elem.ValidationError)
-
-    # str can't be used :
-    try:
-        elem = Elem()
-        elem.add_content(['', ])
-        raise (Exception("incorrect behaviour."))
-    except Exception as e:
-        assert isinstance(e, Elem.ValidationError)
-
-    try:
-        elem = Elem(content='')
-        raise (Exception("incorrect behaviour."))
-    except Exception as e:
-        assert isinstance(e, Elem.ValidationError)
-    print('Error cases : OK.')
-
-
-def test_embedding():
-    assert (str(Elem(content=Elem(content=Elem(content=Elem()))))
-            == """<div>
-  <div>
-    <div>
-      <div></div>
-    </div>
-  </div>
-</div>""")
-    print('Element embedding : OK.')
-
-
-import traceback
-
 if __name__ == "__main__":
-    try:
-        test_elem_basics()
-        print('Tests succeeded!')
-    except AssertionError as e:
-        traceback.print_exc()
-        print(e)
-        print('Tests failed!')
+    title = Elem(tag='title', content=Text('"Hello ground!"'))
+    h1 = Elem(tag='h1', content=Text('"Oh no, not again!"'))
+    img = Elem(tag='img', attr={'src': "http://i.imgur.com/pfp3T.jpg"}, tag_type='simple')
+    html_page = Elem(tag='html', content=Elem(tag='head', content=title))
+    html_page.add_content(Elem(tag='body', content=[h1, img]))
+    print(html_page)
